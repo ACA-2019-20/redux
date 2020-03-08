693e07d1d5e349611e6b0678dabbf3decc669000
Vrezh Oganisyan <hovanisyan.vrezh@gmail.com>
2020-03-08 17:23:32 +0400

    feat(store): create store arguments

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,33 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers";
 
-// optional
-const initialState = {
-  count: 0
+function logger({ getState }) {
+  return next => action => {
+    console.group(action.type);
+    console.log("state before dispatch", getState());
+    console.log("will dispatch", action);
+
+    const returnValue = next(action);
+
+    console.log("state after dispatch", getState());
+    console.groupEnd();
+    return returnValue;
+  };
+}
+
+const loadStateFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem("counter-app")) || {};
 };
 
-export default createStore(rootReducer, initialState);
+const initialState = loadStateFromLocalStorage();
+
+const store = createStore(rootReducer, initialState, applyMiddleware(logger));
+
+const saveStateToLocalStorage = () => {
+  const state = store.getState();
+  localStorage.setItem("counter-app", JSON.stringify(state));
+};
+
+store.subscribe(saveStateToLocalStorage);
+
+export default store;
